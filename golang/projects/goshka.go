f71073bf7d6259a3f7d777752096f68475c13971
zamasulolmeme123 <bezoyan.roma@gmail.com>
2026-03-21 20:04:49 +0600

    Goshka project

---
+++ golang/projects/goshka.go
@@ -0,0 +1,108 @@
+package main 
+
+import (
+	"fmt"
+	//"math/rand/v2"
+	"bufio"
+	"os"
+	"strconv"
+	"time"
+)
+
+
+// TO DO
+// Step 1: Implement your Player struct and use the sort.Slice (or the newer slices.SortFunc) to sort them.
+// Step 2: Wrap your Linear and Binary searches in Goroutines and use a channel to send the result back to main.
+// Step 3: Write a simple Unit Test for your binarySearch function to make sure it handles edge cases (like an empty slice or a missing number).
+
+
+
+
+
+
+
+
+
+
+
+func main() {
+	fmt.Println("Hello!")
+	fmt.Println("Let's play with algorithms!")
+
+	//Scanner 
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("First of all let's define the size of our slice")
+	fmt.Print("Type your size of slice here(int only): ")
+	if ok := scanner.Scan(); !ok {
+			fmt.Println("Error!")
+			return
+	} else if scanner.Text() == "" || scanner.Text() == " "{
+			fmt.Println("Exiting program....")
+			return
+	}
+
+	size, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		fmt.Println("Error!")
+		return
+	}
+
+	// Creating slices
+	sliceIntBinary := []int{}
+	sliceStringBinary := []string{"a", "b", "c", "d", "e", "f", "g", "h", "q", "z",}
+
+	for i := 0; i < size; i++ {
+		sliceIntBinary = append(sliceIntBinary, i)
+		sliceStringBinary = append(sliceStringBinary, string(i))
+	}
+
+	// Binary search
+	for {
+		fmt.Println("Okay let's start binary search with integers")
+		fmt.Print("Type your number for binary search: ")
+
+		if ok := scanner.Scan(); !ok {
+			fmt.Println("Error!")
+			return
+		} else if scanner.Text() == "" || scanner.Text() == " "{
+			fmt.Println("Exiting program....")
+			return
+		}
+
+		numberGuessBinary, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println("Error!")
+			return
+		}
+
+		binarySearch(sliceIntBinary, numberGuessBinary)
+	}
+
+	// Bubble sort
+	
+	// Stack and Recursion
+	
+	// O(n!)
+	
+	// 
+}
+
+func binarySearch(l []int, g int) int{
+	binaryStart := time.Now()
+	high, low := len(l)-1, 0
+	i := 0
+	for low <= high {
+		i += 1
+		mid := low + (high - low) / 2
+		if g == l[mid] {
+			fmt.Printf("Binary search takes %v iterations. Time: %v\n", i, time.Since(binaryStart))
+			return mid
+		} else if g > l[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	fmt.Println("Not finded")
+	return -1
+}
